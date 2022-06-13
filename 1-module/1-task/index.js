334a79e5d5f751aee8105e43ddf0a3f24bed24f1
Valentin715 <justvalk@yandex.ru>
2022-06-14 02:07:04 +0300

    Module 1 tasks

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -3,12 +3,11 @@ function factorial(n) {
   // ваш код...
   let x = 1
   
-  for (;n>0;n--) {
+  for (; n>0; n--) {
     x = x*n; 
   }
   return x
 }
 
-n = prompt("Введите число для вычисления факториала",0)
 
 
